b1cdd6a11ce31a23b591c299697d7758f1c9ef9d
Udita <uditamitra325@gmail.com>
2024-09-03 21:21:18 +0530

    my first commit.

---
+++ src/bank/management/system/Connn.java
@@ -0,0 +1,19 @@
+package bank.management.system;
+
+
+import java.sql.*;
+
+public class Connn{
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Ank#udi*12");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
